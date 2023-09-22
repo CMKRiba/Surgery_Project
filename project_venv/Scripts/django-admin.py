7cf7dbcaf87416418631be547a17487f906df1cd
Clara <145725974+CMKRiba@users.noreply.github.com>
2023-09-22 15:47:59 +0200

    phase1

---
+++ project_venv/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\Mbulelo.Jali\Desktop\HospitalManagement_Django\HospitalManagement_Django\project_venv\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
